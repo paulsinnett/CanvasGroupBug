47817f344d7cb5efbb70aecb9c27ce6483419ad9
Paul Sinnett <paul.sinnett@gmail.com>
2018-07-30 11:43:16 +0100

    Initial commit

---
+++ Assets/CreateButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateButton : MonoBehaviour {
+
+    public GameObject buttonPrefab;
+    public bool workaround = false;
+
+	void Start () {
+		var button = Instantiate(buttonPrefab, transform);
+        if (workaround)
+        {
+            button.SendMessage("OnCanvasGroupChanged");
+        }
+	}
+}
---
+++ Assets/Tests/TestCanvasGroupBug.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TestCanvasGroupBug {
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator TestCanvasGroupAsParentStatic() {
+        yield return SceneManager.LoadSceneAsync("CanvasGroupAsParentStatic");
+        yield return null;
+        var button = GameObject.FindObjectOfType<Button>();
+        Assert.IsFalse(button.IsInteractable());
+    }
+
+    [UnityTest]
+    public IEnumerator TestCanvasGroupAsParentDynamic() {
+        yield return SceneManager.LoadSceneAsync("CanvasGroupAsParentDynamic");
+        yield return null;
+        var button = GameObject.FindObjectOfType<Button>();
+        Assert.IsFalse(button.IsInteractable());
+    }
+
+    [UnityTest]
+    public IEnumerator TestCanvasGroupAsGrandparentStatic() {
+        yield return SceneManager.LoadSceneAsync("CanvasGroupAsGrandparentStatic");
+        yield return null;
+        var button = GameObject.FindObjectOfType<Button>();
+        Assert.IsFalse(button.IsInteractable());
+    }
+
+    [UnityTest]
+    public IEnumerator TestCanvasGroupAsGrandparentDynamic() {
+        yield return SceneManager.LoadSceneAsync("CanvasGroupAsGrandparentDynamic");
+        yield return null;
+        var button = GameObject.FindObjectOfType<Button>();
+        Assert.IsFalse(button.IsInteractable());
+    }
+
+    [UnityTest]
+    public IEnumerator TestCanvasGroupAsGrandparentDynamicWorkaround() {
+        yield return SceneManager.LoadSceneAsync("CanvasGroupAsGrandparentDynamicWorkaround");
+        yield return null;
+        var button = GameObject.FindObjectOfType<Button>();
+        Assert.IsFalse(button.IsInteractable());
+    }
+}
